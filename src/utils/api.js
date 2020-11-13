caccfd3bd14bb9d3102df0db6b98b052406aba32
Martin Lunnegård <martin.lunnegard@gmail.com>
2020-11-13 13:55:15 +0100

    Deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // API credentials
 
 const API_URL_1 = 'https://jobsearch.api.jobtechdev.se/'; 
-const API_URL_2 = 'http://api.adzuna.com/v1/api/jobs/gb/search/1';
+const API_URL_2 = 'https://api.adzuna.com/v1/api/jobs/gb/search/1';
 const API_KEY_1 = 'YidXMVx4YTVceDAzXlBceGE1K1x4MTNqRnRceDkwXHhjMFx4YjhTe1x4YWVceDE1MSc';
 const API_KEY_2 = '535be9d711989a3988a14939c8fb9d7d';
 const API_ID = '5605d2ee';
